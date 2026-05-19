8137023429706e64f1eefacbb7ee9bdc43fc4a2f
Adebayo <adeniranadebayo27@gmail.com>
2026-05-19 10:00:29 +0100

    changes in email port

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -7,8 +7,8 @@ dotenv.config();
 const sendEmail = async ( subject, send_to, message , sent_from, ) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port:587,  
-    secure:false,
+    port:465,  
+    secure:true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS
